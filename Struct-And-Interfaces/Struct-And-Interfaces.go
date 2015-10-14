82573f15d9c68dcfbeed21b41bcdaf075f91b16b
balaprasanna <balaprasannav2009@gmail.com>
2015-10-15 01:46:26 +0800

    Go sleep

--- Struct-And-Interfaces/Struct-And-Interfaces.go
+++ Struct-And-Interfaces/Struct-And-Interfaces.go
@@ -7,6 +7,15 @@ type Circle struct {
 	y float64
 	r float64
 }
+
+func (c *Circle) DoubleYs(){
+	c.x *= 2; c.y *= 2; c.r *= 2;
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.r*c.r	
+}
+
 func CircleArea(circles ...Circle) []float64 {
 	areas := make([]float64, len(circles), len(circles) * 2)
 	for i,c := range circles {
@@ -21,11 +30,23 @@ func main() {
 
 	cPtr := new(Circle)
 	cPtr = &c1
-	localC := *cPtr;
-	fmt.Println("X:",localC.x, "Y:",localC.y)
-	fmt.Println(CircleArea(c1,c2, *cPtr))
+	c := *cPtr
 
+	fmt.Println(CircleArea(c1,c2, c))
+	fmt.Println(cPtr.Area())
+	fmt.Println(c1.Area())
+	fmt.Println(c2.Area())
 
+	//fmt.Println("Before =",c1, "After =", c1.DoubleYs())
+	fmt.Println("Before =", *cPtr); 
+	cPtr.DoubleYs();
+	fmt.Println("After =", *cPtr);
+
+	fmt.Println("Before =", c2); 
+	c2.DoubleYs();
+	fmt.Println("After =", c2);
+	//fmt.Println("Before =",c1); c1.DoubleYs(); fmt.Println("After =", c1);
+	
 	// *cPtr.x = 10.0
 	// *cPtr.y = 20.0
 	// *cPtr.r = 5.0
